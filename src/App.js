749eb41d199beb7ff147751a72fee1db1bdefc8d
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-01-31 17:51:38 +0100

    adding task component to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Task from './Task';
+
 
 class App extends Component {
   render() {
     return (
-      <div>Hello world</div>
+      <Task />
     );
   }
 }
